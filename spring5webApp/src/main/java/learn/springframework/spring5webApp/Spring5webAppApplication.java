35557116b1540b631a4d1c508ecd24d56ced98ef
Shyamsunder Yadav <y.shyam287@gmail.com>
2019-10-27 18:27:31 +0530

    basic jpa example

---
+++ spring5webApp/src/main/java/learn/springframework/spring5webApp/Spring5webAppApplication.java
@@ -0,0 +1,16 @@
+package learn.springframework.spring5webApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5webAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5webAppApplication.class, args);
+		System.out.println("######################################");
+		System.out.println("hello world!!!!!!!!!!!!!!!!!!");
+		System.out.println("######################################");
+	}
+
+}
